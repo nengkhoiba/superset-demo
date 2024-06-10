9ffc4acb0ae61e912ae3131873852308c455520e
Nengkhoiba Chungkham <nengkhoiba@Nengkhoibas-MacBook-Pro.local>
2024-06-10 12:05:55 +0530

    update

--- readme.md
+++ readme.md
@@ -1,3 +1,7 @@
 ## Apache superset driver link
 
 https://superset.apache.org/docs/configuration/databases/
+
+## Running
+
+`docker-compose up`
--- superset/superset_config.py
+++ superset/superset_config.py
@@ -1,6 +1,21 @@
 FEATURE_FLAGS = {
     "ENABLE_TEMPLATE_PROCESSING": True,
 }
-
+SESSION_COOKIE_SAMESITE = None
 ENABLE_PROXY_FIX = True
-SECRET_KEY = "YOUR_OWN_RANDOM_GENERATED_STRING"
+PUBLIC_ROLE_LIKE_GAMMA = True
+FEATURE_FLAGS = {
+    "EMBEDDED_SUPERSET": True
+}
+SECRET_KEY = "jkhsdhuh23oeh8y238ehjonwh"
+CORS_OPTIONS = {
+  'supports_credentials': True,
+  'allow_headers': ['*'],
+  'resources':['*'],
+  'origins': ['http://superset:8088','http://localhost:8088']
+}
+OVERRIDE_HTTP_HEADERS = {'X-Frame-Options': 'ALLOWALL'}
+TALISMAN_ENABLED = False
+ENABLE_CORS = True
+HTTP_HEADERS={"X-Frame-Options":"ALLOWALL"}
+WTF_CSRF_ENABLED = False
